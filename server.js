17adc7ab4594d20b0ce5d04150a4cb052a4a4e14
Rishi Sathe <rishikesh.sathe01@gmail.com>
2026-03-10 18:12:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 app.get("/", (req, res) => {
-  res.send("Hello from Node.js!");
+  res.send("Hello from Node.js! - Version 1");
 });
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
